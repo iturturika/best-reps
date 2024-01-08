2e4e86dee7f3abed3e2eaf4631a03706e3b8f9f1
ozzy <iturturika89@gmail.com>
2024-01-08 12:03:39 +0200

    добавил аналитику гугл

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './global.scss'
-
+import Script from 'next/script'
 export const metadata = {
   title: 'Best-Reps',
   description: 'Каталог лучших реплик во всем мире.',
@@ -8,6 +8,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <body>{children}</body>
     </html>
   )
